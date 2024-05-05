d84b709afe4ec351e93f90de8fa1600a6609b4aa
Sinan <sinanyavuzylmaz@gmail.com>
2024-05-05 17:33:53 +0300

    "adjustments"

--- 02-clock/script.js
+++ 02-clock/script.js
@@ -8,12 +8,12 @@ function setDate() {
   const seconds = now.getSeconds()
   const secondsDegrees = (seconds / 60) * 360 + 90
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-  console.log(seconds)
   const mins = now.getMinutes()
   const minsDegrees = (mins / 60) * 360 + 90
   minsHand.style.transform = `rotate(${minsDegrees}deg)`
   const hours = now.getHours()
   const hoursDegrees = (hours / 12) * 360 + 90
+
   hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
   const resetClock = () => {
     if (seconds == 0) {
