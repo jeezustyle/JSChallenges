d68dad1109ad7a7df59b7b60526296ce3e6415df
Sinan <sinanyavuzylmaz@gmail.com>
2024-05-06 23:06:59 +0300

    "initial commit"

---
+++ 03-background-color/script.js
@@ -0,0 +1,23 @@
+const red = document.getElementById('red')
+const green = document.getElementById('green')
+const blue = document.getElementById('blue')
+const inputs = document.querySelectorAll('input')
+
+const colorBodyChange = () => {
+  let rgb = [...inputs].reduce((acc, curr) => {
+    return [...acc, localStorage.getItem(curr.id) || value]
+  }, [])
+  document.body.style.backgroundColor = `rgb(${rgb.join(',')})`
+}
+
+inputs.forEach((input) => {
+  input.value = localStorage.getItem(input.id) || input.value
+  input.nextElementSibling.innerText = input.value
+  input.addEventListener('input', () => {
+    localStorage.setItem(input.id, input.value)
+    input.nextElementSibling.innerText = input.value
+    colorBodyChange()
+  })
+})
+
+colorBodyChange()
